fd5daba71618e5c3ada9c360e5951d6a6f0f5f48
kimhyeonjoo22 <20242406@sdhs.gwe.hs.kr>
2024-11-18 14:54:24 +0900

    Update and rename Streamlit to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 #이전 수업 시간에 만들었던 이미지 분류 pkl 파일을 바탕으로 한 이미지 분류 모델을 Streamlit에 올리는 예제 코드
+#파일 이름 streamlit_app.py
+
 import streamlit as st
 from fastai.vision.all import *
 from PIL import Image
@@ -49,12 +51,12 @@ if uploaded_file is not None:
     # 클래스별 확률을 HTML과 CSS로 시각화
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
-    # if prediction == labels[0]:
-    #     st.write("중냉 꿋굿")
-    # elif prediction == labels[1]:
-    #     st.write("짜장면은 굿")
-    # elif prediction == labels[2]:
-    #     st.write("짬뽕은 맵지만 맛있어!!")
+    if prediction == labels[0]:
+         st.write("중냉 꿋굿")
+    elif prediction == labels[1]:
+         st.write("짜장면은 굿")
+    elif prediction == labels[2]:
+         st.write("짬뽕은 맵지만 맛있어!!")
 
     for label, prob in zip(labels, probs):
         # HTML 및 CSS로 확률을 시각화
